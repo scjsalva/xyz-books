600473489a65d15fc33cf77401e61aa660e06291
John Salva <scjsalva@gmail.com>
2024-09-24 16:06:11 +0800

    Configure Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './app/**/*.html.erb',
+      './app/helpers/**/*.rb',
+      './app/javascript/**/*.js',
+      './app/views/**/*.html.erb',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
