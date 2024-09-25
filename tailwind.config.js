d7119e873c03c595cc7a95d671d57793fe03f5ed
John Salva <scjsalva@gmail.com>
2024-09-25 13:27:23 +0800

    Add home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+      './app/**/**/*.css',
       './app/**/*.html.erb',
       './app/helpers/**/*.rb',
       './app/javascript/**/*.js',
       './app/views/**/*.html.erb',
     ],
     theme: {
-      extend: {},
+      extend: {
+        screens: {
+          lg: '1400px',
+          rg: '1000px',
+        },
+        width: {
+          'dscreen': '100vdw',
+        },
+        height: {
+          'dscreen': '100vdh',
+        },
+      },
     },
     plugins: [],
   };
