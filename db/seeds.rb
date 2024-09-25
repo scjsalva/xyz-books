9fc06007e07136d8693b5e9fce5525e6a99b59ea
John Salva <scjsalva@gmail.com>
2024-09-25 18:45:45 +0800

    Fix duplicate book on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,6 @@ authors = [
   { first_name: "Balazs", last_name: "Lorinczi" },
   { first_name: "Peter", last_name: "Hoey" },
   { first_name: "Maria", last_name: "Hoey" },
-  { first_name: "Jeff", last_name: "Lemire" },
 ].map { |auth| Author.find_or_create_by!(auth) }
 
 # Books
@@ -42,16 +41,6 @@ books = [
     publication_year: 2022,
     edition: "Book 1",
     cover_url: "The Bend of Luck.png",
-    publisher: publishers[4] # Top Shelf Productions
-  },
-  {
-    title: "The Underwater Welder",
-    isbn_13: "978-1-603090-74-2",
-    isbn_10: "1-603-09074-6",
-    price: 4170.00,
-    publication_year: 2012,
-    edition: "Book 1",
-    cover_url:  "The Underwater Welder.png",
     publisher: publishers[4] # Top Shelf Productions
   },
   {
@@ -97,6 +86,7 @@ books = [
     price: 3000.00,
     publication_year: 2022,
     edition: nil,
+    cover_url:  "The Underwater Welder.png",
     publisher: publishers[3] # McSweeney's
   }
 ].map { |book| Book.find_or_create_by!(book) }
@@ -107,17 +97,15 @@ AuthorBook.find_or_create_by!(author: authors[7], book: books[0]) # Balazs Balaz
 AuthorBook.find_or_create_by!(author: authors[8], book: books[1]) # Peter Hoey - The Bend of Luck
 AuthorBook.find_or_create_by!(author: authors[9], book: books[1]) # Maria Hoey - The Bend of Luck
 
-AuthorBook.find_or_create_by!(author: authors[10], book: books[2]) # Jeff Lemire - The Underwater Welder
+AuthorBook.find_or_create_by!(author: authors[0], book: books[2]) # Joel Hartse - American Elf
+AuthorBook.find_or_create_by!(author: authors[1], book: books[2]) # Hannah Templer - American Elf
+AuthorBook.find_or_create_by!(author: authors[2], book: books[2]) # Marguerite Duras - American Elf
 
-AuthorBook.find_or_create_by!(author: authors[0], book: books[3]) # Joel Hartse - American Elf
-AuthorBook.find_or_create_by!(author: authors[1], book: books[3]) # Hannah Templer - American Elf
-AuthorBook.find_or_create_by!(author: authors[2], book: books[3]) # Marguerite Duras - American Elf
+AuthorBook.find_or_create_by!(author: authors[3], book: books[3]) # Kingsley Amis - Cosmoknights
+AuthorBook.find_or_create_by!(author: authors[3], book: books[4]) # Kingsley Amis - Essex County
 
-AuthorBook.find_or_create_by!(author: authors[3], book: books[4]) # Kingsley Amis - Cosmoknights
-AuthorBook.find_or_create_by!(author: authors[3], book: books[5]) # Kingsley Amis - Essex County
+AuthorBook.find_or_create_by!(author: authors[1], book: books[5]) # Hannah Templer - Hey, Mister (Vol 1)
+AuthorBook.find_or_create_by!(author: authors[4], book: books[5]) # Fannie Flagg - Hey, Mister (Vol 1)
+AuthorBook.find_or_create_by!(author: authors[5], book: books[5]) # Camille Paglia - Hey, Mister (Vol 1)
 
-AuthorBook.find_or_create_by!(author: authors[1], book: books[6]) # Hannah Templer - Hey, Mister (Vol 1)
-AuthorBook.find_or_create_by!(author: authors[4], book: books[6]) # Fannie Flagg - Hey, Mister (Vol 1)
-AuthorBook.find_or_create_by!(author: authors[5], book: books[6]) # Camille Paglia - Hey, Mister (Vol 1)
-
-AuthorBook.find_or_create_by!(author: authors[6], book: books[7]) # Rainer Rilke - The Underwater Welder
+AuthorBook.find_or_create_by!(author: authors[6], book: books[6]) # Rainer Rilke - The Underwater Welder
